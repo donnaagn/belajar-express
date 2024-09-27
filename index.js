5bd4e388260369127e47bddbf5e73a083ff3d9dd
TWCS911 <161106016+donnaagn@users.noreply.github.com>
2024-09-27 11:20:19 +0700

    update

--- index.js
+++ index.js
@@ -2,21 +2,39 @@ const express = require("express")// import modul express
 const app = express()//isisalisasi exprezs
 const port = 3000 //port
 
+app.set('view engine','ejs');//supaya ejs nya dikenali 
+
+
 //route/
 app.get("/",(req,res) =>{
     //res.send("hello");
-    res.sendFile(__dirname + "/index.html");
+    //res.sendFile(__dirname + "/index.html");
+
+    const berita =[
+        {
+            judul: "Berita 1",
+            isi:"isi berita 1"
+        },
+        {
+            judul:"Berita 2",
+            isi: "Isi Berita 2"
+        },
+    ];
+
+    res.render('index',{title: 'Halaman Home',berita});
 });
 
 //route/about
 app.get("/about",(req,res) => {
     //res.send("About us");
-    res.sendFile(__dirname + "/about.html");
+    //res.sendFile(__dirname + "/about.html");
+    res.render('about')
 });
 //route/contact
 app.get("/contact",(req,res) => {
     //res.send("Contact Us");
-    res.sendFile(__dirname + "/contact.html");
+   // res.sendFile(__dirname + "/contact.html");
+    res.render('contact')
 });
 //route/mahasiswa
 app.get("/mahasiswa",(req,res) => {
@@ -42,6 +60,42 @@ app.get("/dosen",(req,res) => {
         
 
     })
+});
+app.get("/prodi",(req,res) => {
+    
+        const prodi =[
+        {
+            namaprodi: "Sistem Informasi",
+            fakultas:"FIKR",
+            singkatan:"SI"
+        },
+        {
+            namaprodi: "Informatika",
+            fakultas:"FIKR",
+            singkatan:"IF"
+        },
+        {
+            namaprodi: "Teknik Elektro",
+            fakultas:"FIKR",
+            singkatan:"TE"
+        },
+        {
+            namaprodi: "Manajemen Informatika",
+            fakultas:"FIKR",
+            singkatan:"MI"
+        },
+        {
+            namaprodi: "Manajemen",
+            fakultas:"FEB",
+            singkatan:"MJ"
+        },
+        {
+            namaprodi: "Akuntansi",
+            fakultas:"FEB",
+            singkatan:"AK"
+        },
+    ];
+    res.render('prodi',{title: 'Halaman Home',prodi});
 });
 //handle route
 app.use("/",(req,res)=>{
